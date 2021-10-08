f1708b7a1d1eceabc8f1220093d831b2aa227e98
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-10-08 22:12:02 +0300

    текст

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/gameFragment.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/gameFragment.java
@@ -1,6 +1,10 @@
 package com.sofiamarchinskaya.moretechmobile;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +12,19 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+
+
 public class gameFragment extends Fragment {
+    private TextView showAllText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View result=inflater.inflate(R.layout.game_fragment, container, false);
+        showAllText = result.findViewById(R.id.view_rules);
+        Spannable span = new SpannableString(getString(R.string.view_all));
+        span.setSpan(new ForegroundColorSpan(Color.parseColor("#3A83F1")), 0, showAllText.getText().length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        showAllText.setText(span);
         return result;
     }
 }
