09ff37f59370b752277d2f794fe675eb567b1eef
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2021-10-08 21:00:58 +0300

    create HomepagePresenter.java

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/HomepagePresenter.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/HomepagePresenter.java
@@ -1,4 +1,14 @@
 package com.sofiamarchinskaya.moretechmobile;
 
 public class HomepagePresenter {
+    private ViewHomepage viewHomepage;
+
+    public HomepagePresenter(
+            ViewHomepage viewHomepage) {
+        this.viewHomepage = viewHomepage;
+    }
+
+    interface ViewHomepage{
+
+    }
 }
