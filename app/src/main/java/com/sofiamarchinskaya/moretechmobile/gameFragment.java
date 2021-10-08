9cca4d5c75dd9251edf8c09acfb058c7ff895888
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-10-08 19:13:14 +0300

    fragment

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/gameFragment.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/gameFragment.java
@@ -1,4 +1,18 @@
 package com.sofiamarchinskaya.moretechmobile;
 
-public class gameFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class gameFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View result=inflater.inflate(R.layout.game_fragment, container, false);
+        return result;
+    }
 }
