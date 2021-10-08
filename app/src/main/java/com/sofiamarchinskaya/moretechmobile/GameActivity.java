e4e7c6e34c6909874f86aa7b38fb557f43a11243
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-10-09 01:45:44 +0300

    dots and text

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/GameActivity.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/GameActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class GameActivity extends AppCompatActivity {
     private List<View> topDots;
     private View dot;
     private LinearLayout dotsLayout;
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +28,22 @@ public class GameActivity extends AppCompatActivity {
         topTextView.setText("Шаг  "+preferences.getString(Constant.YEAR,"1")+" из 10");
         dotsLayout = findViewById(R.id.dots_layout);
         topDots = new ArrayList<>();
+        int dip = 9;
+        int margin = 6;
+        //перевод в dp
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip,  getResources().getDisplayMetrics());
+        LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                (int)px, (int)px);
+        float m_px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, margin,  getResources().getDisplayMetrics());
 
-        for(int i = 0; i<10; i++){
+       for(int i = 0; i<10; i++){
 
             dot=new View(this);
             dot.setBackgroundResource(R.drawable.ic_circle);
-            dot.getLayoutParams().height=9;
-            dot.getLayoutParams().width=9;
+           lParams.setMargins(0,0,(int)m_px,0);
+          dot.setLayoutParams(lParams);
             topDots.add(dot);
-            dotsLayout.addView(dot);
+            dotsLayout.addView(dot,lParams);
         }
 
 
