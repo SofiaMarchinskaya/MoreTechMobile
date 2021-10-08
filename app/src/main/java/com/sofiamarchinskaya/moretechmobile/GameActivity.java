ad5dbf090f5db7bc305aff0c17e85ea60ed46f37
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-10-09 00:14:13 +0300

    game activity

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/GameActivity.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/GameActivity.java
@@ -3,12 +3,15 @@ package com.sofiamarchinskaya.moretechmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
-
+    private TextView topTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        topTextView = findViewById(R.id.top_text);
     }
 }
