32db91f51e9f55739179349a6561e188eae4ecb8
Marchinskaya Sofia <sonamarcinskaa@gmail.com>
2021-10-09 19:05:53 +0300

    fragment

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/BudgetCard.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/BudgetCard.java
@@ -5,12 +5,33 @@ public class BudgetCard {
     private final String title;
     private final int Description;
     private final int price;
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDescription() {
+        return Description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getHappy_percent() {
+        return happy_percent;
+    }
+
     private final int happy_percent;
     BudgetCard(int image, String title,int description, int price, int happy_percent){
 
         this.image = image;
         this.title = title;
-        Description = description;
+        this.Description = description;
         this.price = price;
         this.happy_percent = happy_percent;
     }
--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/BudgetFragment.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/BudgetFragment.java
@@ -7,9 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,6 +21,9 @@ private View acceptButton;
 private View abortButton;
 private ImageView img;
 private TextView title;
+private TextView price;
+private TextView happyPercent;
+private Button finalButton;
     private TextView description;
     int counter;
 
@@ -26,7 +31,8 @@ private TextView title;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View result = inflater.inflate(R.layout.game_fragment, container, false);
+        View result = inflater.inflate(R.layout.fragment_budget, container, false);
+        cards = new ArrayList<>();
         cards.add(new BudgetCard(R.drawable.ic_wallet,"Минимальные траты" +
                 "на жизнь за год",R.string.min_trati,Constant.PRICE_FOR_YEAR,16));
         cards.add(new BudgetCard(R.drawable.ic_car,"Путешествие " +
@@ -40,15 +46,59 @@ private TextView title;
         cards.add(new BudgetCard(R.drawable.ic_tick_square,"Основные решения" +
                 "приняты",R.string.final_card,Constant.PRICE_FOR_YEAR,16));
         counter=0;
+
     acceptButton = result.findViewById(R.id.tick_square);
+    finalButton = result.findViewById(R.id.finalButton);
+    finalButton.setVisibility(View.INVISIBLE);
     abortButton = result.findViewById(R.id.close_square);
+    price = result.findViewById(R.id.price_on_card);
+    happyPercent = result.findViewById(R.id.happy_on_card);
     img = result.findViewById(R.id.image_on_card);
     title= result.findViewById(R.id.title_on_card);
     description = result.findViewById(R.id.subtitle_on_card);
+    abortButton.setVisibility(View.INVISIBLE);
+
+//    img.setBackgroundResource(0);
+      img.setBackgroundResource(cards.get(counter).getImage());
+    title.setText(cards.get(counter).getTitle());
+    description.setText(cards.get(counter).getDescription());
+    happyPercent.setText(cards.get(counter).getHappy_percent()+"");
+    price.setText(cards.get(counter).getPrice()+"");
+
+
     BudgetCard card;
     acceptButton.setOnClickListener(view -> {
-
+        counter++;
+        abortButton.setVisibility(View.VISIBLE);
+        img.setBackgroundResource(cards.get(counter).getImage());
+        title.setText(cards.get(counter).getTitle());
+        description.setText(cards.get(counter).getDescription());
+        happyPercent.setText(cards.get(counter).getHappy_percent()+"");
+        price.setText(cards.get(counter).getPrice()+"");
+        if(counter==cards.size()-1 ){
+            abortButton.setVisibility(View.INVISIBLE);
+            acceptButton.setVisibility(View.INVISIBLE);
+            price.setVisibility(View.INVISIBLE);
+            happyPercent.setVisibility(View.INVISIBLE);
+            finalButton.setVisibility(View.VISIBLE);
+        }
     });
+    abortButton.setOnClickListener(view -> {
+        counter++;
+        img.setBackgroundResource(cards.get(counter).getImage());
+        title.setText(cards.get(counter).getTitle());
+        description.setText(cards.get(counter).getDescription());
+        happyPercent.setText(cards.get(counter).getHappy_percent()+"");
+        price.setText(cards.get(counter).getPrice()+"");
+        if(counter==cards.size()-1 ){
+            abortButton.setVisibility(View.INVISIBLE);
+            acceptButton.setVisibility(View.INVISIBLE);
+            price.setVisibility(View.INVISIBLE);
+            happyPercent.setVisibility(View.INVISIBLE);
+            finalButton.setVisibility(View.VISIBLE);
+        }
+    });
+
 
         return result;
     }
