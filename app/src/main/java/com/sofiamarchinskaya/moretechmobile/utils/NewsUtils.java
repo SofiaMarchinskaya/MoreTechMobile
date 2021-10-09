e3fcb25d1bf4452e8f1847a8d48e75914d28d095
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2021-10-09 23:22:35 +0300

    fix

--- app/src/main/java/com/sofiamarchinskaya/moretechmobile/utils/NewsUtils.java
+++ app/src/main/java/com/sofiamarchinskaya/moretechmobile/utils/NewsUtils.java
@@ -17,28 +17,38 @@ public class NewsUtils {
     private static Set<News> currentNews = new ArraySet<>();
 
     public static void init(){
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
-        allNews.add(new News("News id good", Constant.GOOD_NEWS, "Alibaba"));
-        allNews.add(new News("News is bad", Constant.BAD_NEWS, "Alibaba"));
+        allNews.add(new News("В третем квартале этого года более миллиона пользователей не смогли" +
+                " войти в своми акканты в Instagram и WhatApp. Данный технический сбой болезнено " +
+                "сказался на мировой экономике.", Constant.BAD_NEWS, "Instagram"));
+        allNews.add(new News("Intel анансоривала выход собсвенной игры в 2023 году. Она будет " +
+                "расчитана под их новую игровую карту", Constant.GOOD_NEWS, "Intel"));
+        allNews.add(new News("По расчетам астрологов ретроградный Меркурий переходит в фазу " +
+                "Венеры, поэтому следующий год обещает быть удачным для роста выработки нефти и " +
+                "добычи газа.  id good", Constant.GOOD_NEWS, "Лукоил"));
+        allNews.add(new News("В третем квартале этого года более миллиона пользователей не смогли" +
+                " войти в своми акканты в Instagram и WhatApp. Данный технический сбой болезнено " +
+                "сказался на мировой экономике.", Constant.BAD_NEWS, "Instagram"));
+        allNews.add(new News("Intel анансоривала выход собсвенной игры в 2023 году. Она будет " +
+                "расчитана под их новую игровую карту", Constant.GOOD_NEWS, "Intel"));
+        allNews.add(new News("По расчетам астрологов ретроградный Меркурий переходит в фазу " +
+                "Венеры, поэтому следующий год обещает быть удачным для роста выработки нефти и " +
+                "добычи газа.  id good", Constant.GOOD_NEWS, "Лукоил"));
+        allNews.add(new News("В третем квартале этого года более миллиона пользователей не смогли" +
+                " войти в своми акканты в Instagram и WhatApp. Данный технический сбой болезнено " +
+                "сказался на мировой экономике.", Constant.BAD_NEWS, "Instagram"));
+        allNews.add(new News("Intel анансоривала выход собсвенной игры в 2023 году. Она будет " +
+                "расчитана под их новую игровую карту", Constant.GOOD_NEWS, "Intel"));
+        allNews.add(new News("По расчетам астрологов ретроградный Меркурий переходит в фазу " +
+                "Венеры, поэтому следующий год обещает быть удачным для роста выработки нефти и " +
+                "добычи газа.  id good", Constant.GOOD_NEWS, "Лукоил"));
+        allNews.add(new News("В третем квартале этого года более миллиона пользователей не смогли" +
+                " войти в своми акканты в Instagram и WhatApp. Данный технический сбой болезнено " +
+                "сказался на мировой экономике.", Constant.BAD_NEWS, "Instagram"));
+        allNews.add(new News("Intel анансоривала выход собсвенной игры в 2023 году. Она будет " +
+                "расчитана под их новую игровую карту", Constant.GOOD_NEWS, "Intel"));
+        allNews.add(new News("По расчетам астрологов ретроградный Меркурий переходит в фазу " +
+                "Венеры, поэтому следующий год обещает быть удачным для роста выработки нефти и " +
+                "добычи газа.  id good", Constant.GOOD_NEWS, "Лукоил"));
         nextYear();
     }
 
